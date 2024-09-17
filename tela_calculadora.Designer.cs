7d6d13ae90220f95767a2129af02178d180b1145
Gabriel-Felipe07 <gabriel.belentani217@gmail.com>
2024-09-17 13:53:36 -0300

    ADD: adicionando o arquivo de design da calculadora.

--- tela_calculadora.Designer.cs
+++ tela_calculadora.Designer.cs
@@ -28,12 +28,298 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "tela_calculadora";
+            label1 = new Label();
+            button4 = new Button();
+            button5 = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            button19 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16F);
+            label1.Location = new Point(154, 68);
+            label1.Name = "label1";
+            label1.Size = new Size(103, 30);
+            label1.TabIndex = 0;
+            label1.Text = "resultado";
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Microsoft Sans Serif", 8.25F);
+            button4.Location = new Point(22, 289);
+            button4.Name = "button4";
+            button4.Size = new Size(38, 35);
+            button4.TabIndex = 1;
+            button4.Text = "0";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 16F);
+            button5.Location = new Point(66, 289);
+            button5.Name = "button5";
+            button5.Size = new Size(38, 35);
+            button5.TabIndex = 1;
+            button5.Text = ".";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Microsoft Sans Serif", 8.25F);
+            button1.Location = new Point(22, 248);
+            button1.Name = "button1";
+            button1.Size = new Size(38, 35);
+            button1.TabIndex = 1;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button4_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Microsoft Sans Serif", 8.25F);
+            button2.Location = new Point(66, 248);
+            button2.Name = "button2";
+            button2.Size = new Size(38, 35);
+            button2.TabIndex = 1;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Microsoft Sans Serif", 8.25F);
+            button3.Location = new Point(110, 248);
+            button3.Name = "button3";
+            button3.Size = new Size(38, 35);
+            button3.TabIndex = 1;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button4_Click;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Microsoft Sans Serif", 8.25F);
+            button6.Location = new Point(22, 207);
+            button6.Name = "button6";
+            button6.Size = new Size(38, 35);
+            button6.TabIndex = 1;
+            button6.Text = "4";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button4_Click;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Microsoft Sans Serif", 8.25F);
+            button7.Location = new Point(110, 207);
+            button7.Name = "button7";
+            button7.Size = new Size(38, 35);
+            button7.TabIndex = 1;
+            button7.Text = "6";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button4_Click;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Microsoft Sans Serif", 8.25F);
+            button8.Location = new Point(66, 207);
+            button8.Name = "button8";
+            button8.Size = new Size(38, 35);
+            button8.TabIndex = 1;
+            button8.Text = "5";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button4_Click;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Microsoft Sans Serif", 8.25F);
+            button9.Location = new Point(66, 166);
+            button9.Name = "button9";
+            button9.Size = new Size(38, 35);
+            button9.TabIndex = 1;
+            button9.Text = "8";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button4_Click;
+            // 
+            // button10
+            // 
+            button10.Font = new Font("Microsoft Sans Serif", 8.25F);
+            button10.Location = new Point(22, 166);
+            button10.Name = "button10";
+            button10.Size = new Size(38, 35);
+            button10.TabIndex = 1;
+            button10.Text = "7";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button4_Click;
+            // 
+            // button11
+            // 
+            button11.Font = new Font("Microsoft Sans Serif", 8.25F);
+            button11.Location = new Point(110, 166);
+            button11.Name = "button11";
+            button11.Size = new Size(38, 35);
+            button11.TabIndex = 1;
+            button11.Text = "9";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button4_Click;
+            // 
+            // button12
+            // 
+            button12.Font = new Font("Microsoft Sans Serif", 8.25F);
+            button12.Location = new Point(154, 248);
+            button12.Name = "button12";
+            button12.Size = new Size(45, 76);
+            button12.TabIndex = 1;
+            button12.Text = "+";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button4_Click;
+            // 
+            // button13
+            // 
+            button13.Font = new Font("Microsoft Sans Serif", 8.25F);
+            button13.Location = new Point(205, 166);
+            button13.Name = "button13";
+            button13.Size = new Size(45, 35);
+            button13.TabIndex = 1;
+            button13.Text = "÷";
+            button13.UseVisualStyleBackColor = true;
+            button13.Click += button4_Click;
+            // 
+            // button14
+            // 
+            button14.Font = new Font("Microsoft Sans Serif", 8.25F);
+            button14.Location = new Point(154, 207);
+            button14.Name = "button14";
+            button14.Size = new Size(45, 35);
+            button14.TabIndex = 1;
+            button14.Text = "-";
+            button14.UseVisualStyleBackColor = true;
+            button14.Click += button4_Click;
+            // 
+            // button15
+            // 
+            button15.Font = new Font("Microsoft Sans Serif", 8.25F);
+            button15.Location = new Point(110, 289);
+            button15.Name = "button15";
+            button15.Size = new Size(38, 35);
+            button15.TabIndex = 1;
+            button15.Text = "+/-";
+            button15.UseVisualStyleBackColor = true;
+            button15.Click += button4_Click;
+            // 
+            // button16
+            // 
+            button16.Font = new Font("Microsoft Sans Serif", 8.25F);
+            button16.Location = new Point(154, 166);
+            button16.Name = "button16";
+            button16.Size = new Size(45, 35);
+            button16.TabIndex = 1;
+            button16.Text = "√";
+            button16.UseVisualStyleBackColor = true;
+            button16.Click += button4_Click;
+            // 
+            // button17
+            // 
+            button17.Font = new Font("Microsoft Sans Serif", 8.25F);
+            button17.Location = new Point(205, 207);
+            button17.Name = "button17";
+            button17.Size = new Size(45, 35);
+            button17.TabIndex = 1;
+            button17.Text = "x";
+            button17.UseVisualStyleBackColor = true;
+            button17.Click += button4_Click;
+            // 
+            // button18
+            // 
+            button18.Font = new Font("Microsoft Sans Serif", 8.25F);
+            button18.Location = new Point(205, 289);
+            button18.Name = "button18";
+            button18.Size = new Size(45, 35);
+            button18.TabIndex = 1;
+            button18.Text = "=";
+            button18.UseVisualStyleBackColor = true;
+            button18.Click += button4_Click;
+            // 
+            // button19
+            // 
+            button19.Font = new Font("Microsoft Sans Serif", 8.25F);
+            button19.Location = new Point(205, 248);
+            button19.Name = "button19";
+            button19.Size = new Size(45, 35);
+            button19.TabIndex = 1;
+            button19.Text = "%";
+            button19.UseVisualStyleBackColor = true;
+            button19.Click += button4_Click;
+            // 
+            // tela_calculadora
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(296, 342);
+            Controls.Add(button5);
+            Controls.Add(button14);
+            Controls.Add(button16);
+            Controls.Add(button19);
+            Controls.Add(button18);
+            Controls.Add(button17);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button7);
+            Controls.Add(button15);
+            Controls.Add(button3);
+            Controls.Add(button10);
+            Controls.Add(button6);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(button4);
+            Controls.Add(label1);
+            Name = "tela_calculadora";
+            Text = "tela_calculadora";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button button4;
+        private Button button5;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private Button button19;
     }
 }
--- tela_splash.Designer.cs
+++ tela_splash.Designer.cs
@@ -32,6 +32,9 @@
             progressBar1 = new ProgressBar();
             lbl_frases_carregamento = new Label();
             timer1 = new System.Windows.Forms.Timer(components);
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // progressBar1
@@ -55,11 +58,33 @@
             // 
             timer1.Enabled = true;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.engrenagem;
+            pictureBox1.Location = new Point(284, 255);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(212, 155);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16F);
+            label1.Location = new Point(273, 48);
+            label1.Name = "label1";
+            label1.Size = new Size(258, 30);
+            label1.TabIndex = 3;
+            label1.Text = "Carregando calculadora...";
+            // 
             // tela_splash
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
             Controls.Add(lbl_frases_carregamento);
             Controls.Add(progressBar1);
             FormBorderStyle = FormBorderStyle.None;
@@ -68,6 +93,7 @@
             Text = "Form1";
             FormClosed += Form1_FormClosed;
             Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -77,5 +103,7 @@
         private ProgressBar progressBar1;
         private Label lbl_frases_carregamento;
         private System.Windows.Forms.Timer timer1;
+        private PictureBox pictureBox1;
+        private Label label1;
     }
 }
