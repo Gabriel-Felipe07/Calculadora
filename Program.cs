23fef8cb2374a569b87a3c2e2a5bf648c0f81202
Gabriel-Felipe07 <gabriel.belentani217@gmail.com>
2024-09-17 10:28:05 -0300

    MODIFY: renomeando arquivos

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Calculadora
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new frm_tela_splash());
+            Application.Run(new tela_splash());
         }
     }
 }
