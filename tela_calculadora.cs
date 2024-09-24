4f83d8e229d9053229f62813379e18eb078b9057
Gabriel Belentani Felipe <gabriel.belentani217@gmail.com>
2024-09-24 10:54:16 -0300

    modificando virgula no código

--- tela_calculadora.cs
+++ tela_calculadora.cs
@@ -20,6 +20,44 @@ namespace Calculadora
         private void button4_Click(object sender, EventArgs e)
         {
 
+        private void bt_soma_Click(object sender, EventArgs e)
+        {
+            OperacaoSelecionada = Operacao.Adicao;
+            Valor = Convert.ToDecimal(textbox_resultado.Text);
+            textbox_resultado.Text = "";
+        }
+
+        private void bt_ponto_Click(object sender, EventArgs e)
+        {
+            if (textbox_resultado.Text.Contains(","))
+            {
+                textbox_resultado.Text += ",";
+            }
+        }
+
+        private void bt_igual_Click(object sender, EventArgs e)
+        {
+            switch (OperacaoSelecionada)
+            {
+                case Operacao.Adicao:
+                    Resultado = Valor + Convert.ToDecimal(textbox_resultado.Text);
+                    break;
+                case Operacao.Subtracao:
+                    Resultado = Valor - Convert.ToDecimal(textbox_resultado.Text);
+                    break;
+                case Operacao.Multiplicacao:
+                    Resultado = Valor * Convert.ToDecimal(textbox_resultado.Text);
+                    break;
+                case Operacao.Divisao:
+                    Resultado = Valor / Convert.ToDecimal(textbox_resultado.Text);
+                    break;
+            }
+            textbox_resultado.Text = Convert.ToString(Resultado);
+        }
+
+        private void bt_limpar_Click(object sender, EventArgs e)
+        {
+            textbox_resultado.Text = "";
         }
     }
 }
