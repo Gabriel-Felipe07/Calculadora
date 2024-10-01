e37313a8df1e6bdc09bac6c986dcf3bee3e51c57
Giovana Varussa <giovana.varussa290@gmail.com>
2024-10-01 09:59:13 -0300

    alterando a programação de conversão

--- frm_convertCelsiusToFahrenheit.Designer.cs
+++ frm_convertCelsiusToFahrenheit.Designer.cs
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "convertCelsiusToFahrenheit";
+            SuspendLayout();
+            // 
+            // frm_convertCelsiusToFahrenheit
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Name = "frm_convertCelsiusToFahrenheit";
+            Text = "convertCelsiusToFahrenheit";
+            ResumeLayout(false);
         }
 
         #endregion
