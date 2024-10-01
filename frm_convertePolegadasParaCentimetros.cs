279a467b891acdbde05d6a399a81644eb74cf6fb
Giovana Varussa <giovana.varussa290@gmail.com>
2024-10-01 13:39:51 -0300

    ADD: programação da tela de conversão de medidas de polegadas para centímetros

--- frm_convertePolegadasParaCentimetros.cs
+++ frm_convertePolegadasParaCentimetros.cs
@@ -16,5 +16,31 @@ namespace Calculadora
         {
             InitializeComponent();
         }
+
+        private void button_converter_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(textBox_polegadas.Text, out double polegadas))
+            {
+                double centimetros = polegadas * 2.54;
+                string stringPronta = $"{Convert.ToString(centimetros)}cm";
+                textBox_valor_convertido.Text = stringPronta;
+            }
+            else
+            {
+                MessageBox.Show("Por favor, insira um número válido.");
+            }
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void voltarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tela_calculadora janela_calculadora = new tela_calculadora();
+            this.Hide();
+            janela_calculadora.ShowDialog();
+        }
     }
 }
